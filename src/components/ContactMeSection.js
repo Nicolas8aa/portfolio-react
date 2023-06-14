ff43755a2432d8649577420db1b12cab33d46cfa
Nicolas C <nicolascuellar2305@gmail.com>
2023-06-14 10:36:05 -0500

    Contact me section - Added useFormik configuration - Alert popup when form is submitted - Changing behavior of form inputs to controlled

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import {
   Box,
@@ -12,20 +12,42 @@ import {
   Textarea,
   VStack,
 } from "@chakra-ui/react";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
-import {useAlertContext} from "../context/alertContext";
+import { useAlertContext } from "../context/alertContext";
 
 const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
+  const { isLoading, response, submit } = useSubmit();
   const { onOpen } = useAlertContext();
 
-  const formik = useFormik({
-    initialValues: {},
-    onSubmit: (values) => {},
-    validationSchema: Yup.object({}),
-  });
+  const { errors, getFieldProps, touched, handleSubmit, ...formik } = useFormik(
+    {
+      initialValues: {
+        firstName: "",
+        email: "",
+        type: "",
+        message: "",
+      },
+      onSubmit: async (values) => {
+        await submit("https://jsonplaceholder.typicode.com/posts", values);
+        console.log(values);
+      },
+      validationSchema: Yup.object({
+        firstName: Yup.string().required("Required"),
+        email: Yup.string().email("Invalid email address").required("Required"),
+        type: Yup.string(),
+        message: Yup.string().required("Required"),
+      }),
+    }
+  );
+
+  useEffect(() => {
+    if (response) {
+      onOpen(response.type, response.message);
+    }
+    formik.resetForm();
+  }, [response]);
 
   return (
     <FullScreenSection
@@ -39,28 +61,27 @@ const LandingSection = () => {
           Contact me
         </Heading>
         <Box p={6} rounded="md" w="100%">
-          <form>
+          <form onSubmit={handleSubmit}>
             <VStack spacing={4}>
-              <FormControl isInvalid={false}>
+              <FormControl isInvalid={touched.firstName && errors.firstName}>
                 <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input
-                  id="firstName"
-                  name="firstName"
-                />
-                <FormErrorMessage></FormErrorMessage>
+                <Input id="firstName" {...getFieldProps("firstName")} />
+                <FormErrorMessage>{errors.firstName}</FormErrorMessage>
               </FormControl>
-              <FormControl isInvalid={false}>
+              <FormControl isInvalid={touched.email && errors.email}>
                 <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                />
-                <FormErrorMessage></FormErrorMessage>
+                <Input id="email" type="email" {...getFieldProps("email")} />
+                <FormErrorMessage>{errors.firstName}</FormErrorMessage>
               </FormControl>
               <FormControl>
                 <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type">
+                <Select
+                  id="type"
+                  name="type"
+                  onChange={(e) => {
+                    formik.setFieldValue("type", e.target.value);
+                  }}
+                >
                   <option value="hireMe">Freelance project proposal</option>
                   <option value="openSource">
                     Open source consultancy session
@@ -68,14 +89,14 @@ const LandingSection = () => {
                   <option value="other">Other</option>
                 </Select>
               </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
+              <FormControl isInvalid={touched.message && errors.message}>
+                <FormLabel htmlFor="message">Your message</FormLabel>
                 <Textarea
-                  id="comment"
-                  name="comment"
+                  id="message"
                   height={250}
+                  {...getFieldProps("message")}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{errors.message}</FormErrorMessage>
               </FormControl>
               <Button type="submit" colorScheme="purple" width="full">
                 Submit
